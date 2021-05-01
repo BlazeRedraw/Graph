9fd06cb5321dc582cd4b1a7821393327087fb711
Mario Enrique Muñoz Vindas <65791300+BlazeRedraw@users.noreply.github.com>
2021-04-30 22:44:33 -0600

    Update Relationship.cs

--- Relationship.cs
+++ Relationship.cs
@@ -2,18 +2,102 @@
 {
     internal class Relationship
     {
+        public int tagValue;
+        public string[] movies = new string[200];
+        public string[] tags = new string[200];
+        public Edge edge = new Edge();
+        public Node node = new Node();
         public Node origin;
         public Node end;
-        public Edge edge;
+        public Program program = new Program();
 
         public Relationship()
         {
 
         }
 
-        public string Add()
+        public int Relationships(int i, int j)
         {
-            return "";
+            int z = 0;
+            tagValue = 0;
+
+            foreach (string x in node.movies)
+            {
+                movies[z] = x;
+                z++;
+            }
+
+            origin = node.Find(movies[i], null);
+            end = node.Find(movies[j], null);
+
+            foreach (string x in origin.list)
+            {
+                foreach (string y in end.list)
+                {
+                    if (x == y)
+                    {
+                        tagValue++;
+                    }
+                }
+            }
+
+            return edge.EdgeValue(tagValue);
+        }
+
+        public Node Reccomended()
+        {
+            bool watched = false;
+            int z = 0;
+            int previousValue = 0;
+            tagValue = 0;
+
+            foreach (string x in node.movies)
+            {
+                movies[z] = x;
+                z++;
+            }
+
+            tags = Program.GetList();
+
+            for (int i = 0; i < movies.Length; i++)
+            {
+                if (movies[i] != null)
+                {
+                    for (int x = 0; x < tags.Length; x++)
+                    {
+                        if (movies[i] == tags[x])
+                        {
+                            watched = true;
+                        }
+                    }
+
+                    if (!watched)
+                    {
+                        origin = node.Find(movies[i], null);
+
+                        for (int j = 0; j < tags.Length; j++)
+                        {
+                            foreach (string y in origin.list)
+                            {
+                                if (tags[j] == y)
+                                {
+                                    tagValue++;
+                                }
+                            }
+                        }
+
+                        if (tagValue > previousValue)
+                        {
+                            previousValue = tagValue;
+                            end = origin;
+                        }
+                    }
+                    watched = false;
+                    tagValue = 0;
+                }
+            }
+
+            return end;
         }
     }
 }
