1138a15ae70b6e1d63080b0afc438cce404afe69
Mario Enrique Muñoz Vindas <65791300+BlazeRedraw@users.noreply.github.com>
2021-04-30 22:44:19 -0600

    Update Graph.cs

--- Graph.cs
+++ Graph.cs
@@ -1,19 +1,54 @@
-﻿using System.Collections.Generic;
-
-namespace Graph
+﻿namespace Graph
 {
     internal class Graph
     {
-        public List<Relationship> graph = new List<Relationship>();
+        public Node node = new Node();
+        public Relationship relationship = new Relationship();
 
         public Graph()
         {
 
         }
 
-        public string GraphList()
+        public string GetGraph()
         {
-            return "";
+            string text = "Movies  ";
+
+            for (int i = 0; i < node.movies.Count; i++)
+            {
+                text += " " + (i + 1) + " ";
+            }
+
+            text += "\n";
+
+            for (int i = 0; i < node.movies.Count; i++)
+            {
+                text += (i + 1) + "      - ";
+
+                for (int j = 0; j < node.movies.Count; j++)
+                {
+                    if (i == j)
+                    {
+                        text += 0;
+                    }
+                    else
+                    {
+                        //text += relationship.Relationships(i, j);
+                    }
+
+                    if (j < node.movies.Count - 1)
+                    {
+                        text += ", ";
+                    }
+                    else
+                    {
+                        text += ".\n";
+                    }
+                }
+            }
+
+            return text;
+
         }
     }
 }
